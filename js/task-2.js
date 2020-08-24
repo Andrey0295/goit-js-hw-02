6d2fbe3d84128d1b80631ca9d253c95a851d421c
Andrey0295 <andreevicandrej00@gmail.com>
2020-08-24 20:16:24 +0300

    Делает task_2

--- js/task-2.js
+++ js/task-2.js
@@ -1,15 +1,33 @@
-const calculateEngravingPrice = (message = '', pricePerWord = 0) =>
-  pricePerWord; // Write code in this line
+const calculateEngravingPrice = function (message = '', pricePerWord = 0) {
+  return message.split(' ').length * pricePerWord;
+};
+// Write code in this line
 
-//console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10)); // 80
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    10,
+  ),
+); // 80
 
-//console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 20)); // 160
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    20,
+  ),
+); // 160
 
-//console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40)); // 240
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+); // 240
 
-//console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20)); // 120
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+); // 120
 
-//console.log(calculateEngravingPrice('Uno', 100)); // 100
-let string = 'Привет как дела';
-console.log(string);
-console.log('Привет как дела '.split(' ').length);
+console.log(calculateEngravingPrice('Uno', 100)); // 100
+
+// const calculator = function (mesage = '', cost = 0) {
+//   return mesage.split(' ').length * cost;
+// };
+// console.log(calculator('Hello world i am Gandro', 10));
